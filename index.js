a0ce46a017e22b83a7dda5996118cd2aa292e9d4
Cristina Garcia <garmercy.mar@gmail.com>
2023-10-29 23:54:01 +1100

    adding generatesvg function that creates a svg file. Also its created a shapes js that it's linked with index js

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
-//Declaring fs and downloading inquirer and the version 8.2.4
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
-//Function that contains a readme model that includes the answers by the user.
-function generateMarkdown (answers) {
-    return `
+//GenerateSVG generates a svg logo based in user answers using inquirer
+function generateSVG(answers) {
 
-    //text logo
-    ${answers.text}
-    
-    //text color
-    ${answers.textColor}
-    
-    //shape color
-    ${answers.shapeColor}
-    
-    //shape type
-    ${answers.shape}
-    `;
+  //logoSvgString using svg code it's connected with prompt and will contain the final svg string on line 23 
+  logoSvgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>';
+
+  //shapeResult will be generated depending of shape color options by the user. They are three options: triangle,square or circle.
+  let shapeResult;
+  if (answers.shape === "Triangle") {
+    shapeResult = new Triangle();
+    logoSvgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeResult = new Square();
+    logoSvgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
+  } else {
+    shapeResult = new Circle();
+    logoSvgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
   }
-  
-  //Using prompt the user will answer all the questions that will be included inside the readme file. 
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'text',
+  logoSvgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
+}
+
+//Using prompt the user will answer all the questions that will be included inside the logo svg. 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
         message: 'Please write a text for your logo (Max 3 characters)',
-      },
-      {
-        type: 'input',
-        name: 'textColor',
-        message: 'Write a text color for your logo',
-      },
-      {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Write a shape color for your logo',
-      },
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'What shape would you like to your logo?',
-        choices:['square','triangle','circle'],
-      },
-    ])
-  
-  //This then generates a readme file in another folder.This then connects the user answers and the readme model (inquierer).
-    .then((answers) => {
-      const svsLogo = generateMarkdown(answers);
-      fs.writeFile('./lib/  ', svsLogo, (err) =>
-        err ? console.log(err) : console.log('Successfully created logo svg!')
-      );
-    });
-  
-  
-  
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Write a text color for your logo',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape would you like to your logo?',
+      choices:['Square','Triangle','Circle'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Write a shape color for your logo',
+    },
+  ])
+
+  //This then connects user answers and the final svg string that generates a svg file in another folder
+  .then((answers) => {
+    if (answers.text.length > 3) {
+      console.log("Please your text logo must contain max 3 characters!");
+    }else {
+      const readmeFile = generateSVG(answers);
+      fs.writeFile('./examples/logo.svg', logoSvgString, readmeFile, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg!");
+      });
+    }
+  });
+
